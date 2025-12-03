3cbbaf0cce6dec43bc3ddf8f06e9fee245118ff4
Harry Moss <h.moss@ucl.ac.uk>
2025-12-03 16:22:11 +0000

    Stop putting relative path to an import

--- app/enhancement_processor.py
+++ app/enhancement_processor.py
@@ -11,12 +11,12 @@ from destiny_sdk.references import Reference
 from destiny_sdk.robots import (
     RobotEnhancementBatch,
 )
-from fetching import BasePublisherFetcher
 from loguru import logger
 
 from app.config import Settings
 from app.data_models.generic import APIConfig
 from app.fetch_fulltext import FullTextBatchFetcher, ZeroFullTextsGeneratedError
+from app.fetching import BasePublisherFetcher
 from app.fetching.core import Study, StudyCollection
 
 
